60e7cf06a06fa84b6472a5d2833fe9457d29d61a
JipsonFrancis <bulichombwe99@gmail.com>
2024-01-29 21:05:37 +0200

    Kasi Eats Propped up

--- src/index.js
+++ src/index.js
@@ -1,39 +1,45 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 import './index.css'
+import pizzaData from "./data"
 
 function App ()
 {
-    return (<div className='container'>
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+
+    return (
+    <div className='container'>
         <Header />
         <Menu />
-        <Footer />
-
+        <Footer time={time}/>
     </div>)
 }
 
-const Header = () => (<h1 className="header">Kasi Eats</h1>)
+const Header = () => (<header className="header"><h1>Kasi Eats</h1></header>)
 
-const Menu = () => (<div className="menu">
-<h2>Our Menu</h2>
-<div className="pizzas">
-    <Pizza />
-    <Pizza />
-    <Pizza />
+const Menu = () => (
+<div className="menu">
+    <h2>Our Menu</h2>
+    <div className="pizzas">
+        <Pizza photoUrl={pizzaData[0].photoName} name={pizzaData[0].name} price={pizzaData[0].price} ingredients={pizzaData[0].ingredients} />
+        <Pizza photoUrl={pizzaData[1].photoName} name={pizzaData[1].name} price={pizzaData[1].price} ingredients={pizzaData[1].ingredients} />
+        <Pizza photoUrl={pizzaData[2].photoName} name={pizzaData[2].name} price={pizzaData[2].price} ingredients={pizzaData[2].ingredients} />
+        <Pizza photoUrl={pizzaData[3].photoName} name={pizzaData[3].name} price={pizzaData[3].price} ingredients={pizzaData[3].ingredients} />
+    </div>
+    <Btn />
 </div>
-<Btn />
-</div>)
-
-const Footer = () => ( <footer className="footer">{new Date().toLocaleTimeString()} We're open now</footer> )
-
-const Pizza = () => 
+)
+const Footer = (props) => ( <footer className="footer">{props.time} We're open now</footer> )
+const Pizza = (props) => 
 {
     return (
-        <div className="pizza ">
-            <img src="pizzas/salamino.jpg" alt="Sallamino Pizza"/>
-            <h3>Pizza Salamino</h3>
-            <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-            <span>$18</span>
+        <div className="pizza">
+            <img src={props.photoUrl} alt={props.name}/>
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>${props.price}</span>
+            </div>
         </div>
     )
 }
