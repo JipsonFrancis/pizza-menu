1ab0ce146b7741d17bd3658dc53c5d3ca59544f0
JipsonFrancis <francis.boy.jipson.jnr@gmail.com>
2024-02-01 19:13:31 +0200

    Full UI done

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ function App ()
 {
     const [time, setTime] = useState(new Date().toLocaleTimeString())
 
+    React.useEffect(() => {
+        setInterval( () => {
+            setTime(new Date().toLocaleTimeString())
+        }, 1000)
+    }, [])
+
     return (
     <div className='container'>
         <Header />
@@ -20,27 +26,39 @@ const Header = () => (<header className="header"><h1>Kasi Eats</h1></header>)
 const Menu = () => (
 <div className="menu">
     <h2>Our Menu</h2>
-    <div className="pizzas">
-        <Pizza photoUrl={pizzaData[0].photoName} name={pizzaData[0].name} price={pizzaData[0].price} ingredients={pizzaData[0].ingredients} />
-        <Pizza photoUrl={pizzaData[1].photoName} name={pizzaData[1].name} price={pizzaData[1].price} ingredients={pizzaData[1].ingredients} />
-        <Pizza photoUrl={pizzaData[2].photoName} name={pizzaData[2].name} price={pizzaData[2].price} ingredients={pizzaData[2].ingredients} />
-        <Pizza photoUrl={pizzaData[3].photoName} name={pizzaData[3].name} price={pizzaData[3].price} ingredients={pizzaData[3].ingredients} />
-    </div>
+
+    <ul className="pizzas">
+    {
+        pizzaData.map((pizza) => {
+            return !pizza.soldOut ? <Pizza photoUrl={pizza.photoName} name={pizza.name} price={`$${pizza.price}`} ingredients={pizza.ingredients} /> :
+            <Pizza soldOut="sold-out" photoUrl={pizza.photoName} name={pizza.name} price={`sold out`} ingredients={pizza.ingredients} />
+        })
+    }
+    </ul>
+
     <Btn />
 </div>
 )
-const Footer = (props) => ( <footer className="footer">{props.time} We're open now</footer> )
+const Footer = (props) => 
+{
+    const hour = new Date().getHours()
+    const openHour = 8
+    const closeHour = 20
+    const isOpen = hour >= openHour && hour < closeHour
+
+    return <footer className="footer">{isOpen && `${props.time} We're open now`}</footer>
+}
 const Pizza = (props) => 
 {
     return (
-        <div className="pizza">
-            <img src={props.photoUrl} alt={props.name}/>
+        <li className={`pizza ${props.soldOut}`}>
+            <img className={props.soldOut} src={props.photoUrl} alt={props.name}/>
             <div>
                 <h3>{props.name}</h3>
                 <p>{props.ingredients}</p>
-                <span>${props.price}</span>
+                <span>{props.price}</span>
             </div>
-        </div>
+        </li>
     )
 }
 const Btn = () => (<button className="btn order" type="submit">Order</ button>)
